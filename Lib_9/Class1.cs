5d428b2cfc605cef01a8d218b53e49523d71fdbc
VolodyaHoi <i4masyrov@gmail.com>
2023-10-13 16:00:24 +0300

    upload

---
+++ Lib_9/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_9
+{
+    public class Class1
+    {
+        public int sumOfNumbers(int n) // Сформировать n целых чисел в диапазоне 2-14. Найти сумму чисел < 8. Результат 
+                                       // вывести на экран.
+        {
+            int currentValue = 0; // сгенерированное значение
+            int sum = 0; // сумма
+
+            for (int i = 0; i < n; i++) // формирование n чисел
+            {
+                Random rnd = new Random(); // обьявление рандом
+                currentValue = rnd.Next(2, 14); // создание случайного значения в диапазоне 2 - 14 
+                if (currentValue < 8) // если сгенерированное значение меньше 8,сложить
+                {
+                    sum = sum + currentValue;
+                }
+            }
+            return sum; // возвращение суммы
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_9;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_Start(object sender, RoutedEventArgs e)
+        {
+            int n, result;
+            bool isNum = int.TryParse(tb_Input.Text, out n);
+
+            if(isNum)
+            {
+                Class1 lib = new Class1();
+
+                result = lib.sumOfNumbers(n);
+
+                tb_Output.Text = "Ответ: " + result;
+            } else { tb_Input.Text = "Введенное значение не является числом!";  }
+        }
+
+        private void text_Clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_Input.Text = "";
+        }
+    }
+}
